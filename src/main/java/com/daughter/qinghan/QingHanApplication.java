cfe1cf9690e2c88142a70d22f42888688d4d5b3d
PonyRandom <528525313@qq.com>
2021-08-11 15:28:43 +0800

    提交代码

--- src/main/java/com/daughter/qinghan/QingHanApplication.java
+++ src/main/java/com/daughter/qinghan/QingHanApplication.java
@@ -1,13 +1,16 @@
 package com.daughter.qinghan;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class QingHanApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QingHanApplication.class, args);
+        log.info("本地地址:{}","localhost:8081");
     }
 
 }
--- src/main/java/com/daughter/qinghan/controller/FirstController.java
+++ src/main/java/com/daughter/qinghan/controller/FirstController.java
@@ -1,4 +1,16 @@
 package com.daughter.qinghan.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "qinghan")
 public class FirstController {
+
+
+
+    @RequestMapping(value = "say")
+    public String  qinghanSay(){
+       return "my daughter say Dady";
+    }
 }
