11917a474747c4785b3b85fb61f97f25dd1c1559
PonyRandom <528525313@qq.com>
2021-08-11 14:54:32 +0800

    Initial commit

---
+++ src/main/java/com/daughter/qinghan/QingHanApplication.java
@@ -0,0 +1,13 @@
+package com.daughter.qinghan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingHanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QingHanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daughter/qinghan/QingHanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daughter.qinghan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QingHanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
