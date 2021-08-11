6be2bc4b86b3234cb01166a2f1fa58f89a3151eb
PonyRandom <528525313@qq.com>
2021-08-11 19:42:09 +0800

    增加nacos配置中心

--- src/main/java/com/daughter/qinghan/QingHanApplication.java
+++ src/main/java/com/daughter/qinghan/QingHanApplication.java
@@ -1,5 +1,7 @@
 package com.daughter.qinghan;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import com.alibaba.nacos.spring.context.annotation.discovery.EnableNacosDiscovery;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -8,11 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 @SpringBootApplication
 @MapperScan(basePackages = {"com.daughter.qinghan.mapper"})
+@EnableNacosDiscovery
+@NacosPropertySource(dataId = "qinghan", autoRefreshed = true)
 public class QingHanApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(QingHanApplication.class, args);
         log.info("本地地址:{}","localhost:8081");
     }
-
 }
--- src/main/java/com/daughter/qinghan/controller/FirstController.java
+++ src/main/java/com/daughter/qinghan/controller/FirstController.java
@@ -1,5 +1,6 @@
 package com.daughter.qinghan.controller;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.daughter.qinghan.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +13,12 @@ public class FirstController {
     @Autowired
     private TaskService taskService;
 
+    @NacosValue(value = "${zkz}")
+    private String name;
+
     @RequestMapping(value = "/say")
     public String qinghanSay() {
+        System.out.println(name);
         return "my daughter say Dady";
     }
 
