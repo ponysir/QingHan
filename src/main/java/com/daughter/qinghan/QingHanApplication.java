125a5a77587771d87c90c835180ae7b67af7ce90
PonyRandom <528525313@qq.com>
2021-08-12 10:26:29 +0800

    增加内容

--- src/main/java/com/daughter/qinghan/QingHanApplication.java
+++ src/main/java/com/daughter/qinghan/QingHanApplication.java
@@ -7,12 +7,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @Slf4j
 @SpringBootApplication
 @MapperScan(basePackages = {"com.daughter.qinghan.mapper"})
-@EnableNacosDiscovery
 @NacosPropertySource(dataId = "qinghan", autoRefreshed = true)
-public class QingHanApplication {
+@EnableNacosDiscovery
+public class QingHanApplication  {
     public static void main(String[] args) {
         SpringApplication.run(QingHanApplication.class, args);
         log.info("本地地址:{}","localhost:8081");
